3cfa9e4ba0e55b225223d4df2c82a9247e48b9b4
ouyangwujun <15026901625@139.com>
2017-03-02 16:37:54 +0800

    创建版本

--- ows-disopen-service/src/main/java/com/object/test/disopen/DisOpenRunner.java
+++ ows-disopen-service/src/main/java/com/object/test/disopen/DisOpenRunner.java
@@ -1,10 +1,10 @@
-package com.object.test.open;
+package com.object.test.disopen.service;
 
 /**
  * dubbo run in localhost
  * Created by PUZE81 on 2017/2/27.
  */
-public class Runner {
+public class DisOpenRunner {
     public static void main(String[] args) {
         com.alibaba.dubbo.container.Main.main(args);
     }
--- ows-open-service/src/main/java/com/object/test/open/OpenRunner.java
+++ ows-open-service/src/main/java/com/object/test/open/OpenRunner.java
@@ -1,10 +1,10 @@
-package com.object.test.disopen.service;
+package com.object.test.open;
 
 /**
  * dubbo run in localhost
  * Created by PUZE81 on 2017/2/27.
  */
-public class Runner {
+public class OpenRunner {
     public static void main(String[] args) {
         com.alibaba.dubbo.container.Main.main(args);
     }
