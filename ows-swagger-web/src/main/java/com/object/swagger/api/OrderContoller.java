5a911d81571071ca887fb99f5b1378da4319c46a
ouyangwujun <15026901625@139.com>
2017-06-13 17:41:48 +0800

    swagger_demo

--- ows-swagger-web/src/main/java/com/object/swagger/api/OrderContoller.java
+++ ows-swagger-web/src/main/java/com/object/swagger/api/OrderContoller.java
@@ -2,9 +2,7 @@ package com.object.swagger.api;
 
 import com.object.swagger.api.model.Order;
 import com.object.swagger.api.model.Result;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.*;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,9 +16,11 @@ public class OrderContoller {
 		// 列出某个类目的所有规格
 		@RequestMapping(value = "list", method = RequestMethod.GET)
 		@ApiOperation(value = "获得订单列表", notes = "订单列表信息", httpMethod = "GET")
-		public Result<Order> list(
-				@ApiParam(value = "订单id", required = true) @RequestParam Long id,
-				@ApiParam(value = "订单名称", required = true) @RequestParam String orderName) {
+		@ApiImplicitParams({
+				@ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long"),
+				@ApiImplicitParam(name = "orderName", value = "订单名称", required = true, dataType = "String")
+		})
+		public Result<Order> list(@RequestBody Long id, @RequestBody String orderName) {
 			Result<Order> result = new Result<Order>();
 			Order order = new Order();
 			order.setOrderId(1);
@@ -29,9 +29,12 @@ public class OrderContoller {
 			result.setData(order);
 			return result;
 		}
-		@RequestMapping(value="addOrder",method=RequestMethod.POST)
-		@ApiOperation(value="添加订单",notes="添加订单信息",httpMethod="POST")
-		public int addOrder(Order order){
+		@RequestMapping(value="addOrder", method=RequestMethod.POST)
+		@ApiOperation(value="添加订单", notes="添加订单信息", httpMethod="POST")
+		@ApiImplicitParams({
+				@ApiImplicitParam(name = "order", value = "用户实体", required = true, dataType = "Order")
+		})
+		public int addOrder(@RequestBody Order order){
 			System.out.println("***************"+order.getOrderName());
 			if(order.getOrderName()!=null){
 				return 1;
---
+++ ows-swagger-web/src/main/java/com/object/swagger/controller/Swagger2Controller.java
@@ -0,0 +1,80 @@
+package com.object.swagger.controller;
+
+import com.google.common.base.Optional;
+import com.google.common.base.Strings;
+import io.swagger.annotations.Api;
+import io.swagger.models.Path;
+import io.swagger.models.Swagger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import springfox.documentation.annotations.ApiIgnore;
+import springfox.documentation.service.Documentation;
+import springfox.documentation.spring.web.DocumentationCache;
+import springfox.documentation.spring.web.json.Json;
+import springfox.documentation.spring.web.json.JsonSerializer;
+import springfox.documentation.swagger2.mappers.ServiceModelToSwagger2Mapper;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.HashMap;
+import java.util.jar.JarFile;
+
+/**
+ * 读取接口中的文档信息
+ *      原始的swagger2只能读取实现类中的文档信息
+ * Created by PUZE81 on 2017/3/16.
+ */
+@Controller
+@RequestMapping(value = "/v2")
+public class Swagger2Controller {
+
+    private static final Logger logger = LoggerFactory.getLogger(Swagger2Controller.class.getName());
+
+    public static final String DEFAULT_URL = "/api-docs";
+
+    private static final String HAL_MEDIA_TYPE = "application/hal+json";
+
+    @Value("${springfox.documentation.swagger.v2.host:DEFAULT}")
+    private String hostNameOverride;
+    @Autowired
+    private DocumentationCache documentationCache;
+    @Autowired
+    private ServiceModelToSwagger2Mapper mapper;
+    @Autowired
+    private JsonSerializer jsonSerializer;
+
+    @ApiIgnore
+    @ResponseBody
+    @RequestMapping(value = DEFAULT_URL,method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Json> getDocumentation(
+                @RequestParam(value = "group",required = false) String swaggerGroup,
+                HttpServletRequest servletRequest) {
+        String groupName = (String)Optional.fromNullable(swaggerGroup).or("default");
+        Documentation documentation = this.documentationCache.documentationByGroup(groupName);
+        if(documentation == null) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        } else {
+            Swagger swagger = getSwagger(documentation);
+            return new ResponseEntity(this.jsonSerializer.toJson(swagger), HttpStatus.OK);
+        }
+    }
+
+    private Swagger  getSwagger(Documentation documentation){
+        Swagger swagger = this.mapper.mapDocumentation(documentation);
+        return swagger;
+    }
+}
